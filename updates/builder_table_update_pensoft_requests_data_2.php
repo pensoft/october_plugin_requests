c687a1fb292b970ad1b19b42c12778c06c320b87
root <preprint@pensoft.net>
2025-12-05 11:18:36 +0200

    new fields

---
+++ updates/builder_table_update_pensoft_requests_data_2.php
@@ -0,0 +1,25 @@
+<?php namespace Pensoft\Requests\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftRequestsData2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_requests_data', function($table)
+        {
+            $table->string('status')->nullable();
+            $table->text('experts')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_requests_data', function($table)
+        {
+            $table->dropColumn('status');
+            $table->dropColumn('experts');
+        });
+    }
+}
